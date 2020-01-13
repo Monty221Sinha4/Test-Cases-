7fbef051b6a3065f0e6d56f7d47fdabbc6d39cb3
Monty Sinha <monty.sinha2013@live.co.uk>
2020-01-13 19:04:03 +0530

    Add files via upload

---
+++ Facebool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+namespace SeoulAirport.TestCases
+{
+    [TestClass]
+    public class Facebool
+    {
+        [TestMethod]
+        public void TestSearch()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http//www.facebook.com");
+            IWebElement element = driver.FindElement(By.Name("Q"));
+            element.SendKeys("Hello");
+            element.Submit();
+        }
+    }
+}
